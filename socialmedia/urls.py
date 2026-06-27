afe16acb4e38554e04e6681d9d50bcc6443c61f3
trishaa01 <trishasingh1.08.2005@gmail.com>
2026-06-28 04:04:29 +0530

    Add debug view

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.http import HttpResponse
+import os
+
+def debug_view(request):
+    cloud = os.environ.get('CLOUDINARY_CLOUD_NAME', 'NOT SET')
+    key = os.environ.get('CLOUDINARY_API_KEY', 'NOT SET')
+    storage = os.environ.get('DJANGO_SETTINGS_MODULE', 'NOT SET')
+    return HttpResponse(f"CLOUD: {cloud} | KEY: {key} | SETTINGS: {storage}")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('debug-env/', debug_view),
     path('', include('users.urls')),
     path('', include('posts.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    
-)
